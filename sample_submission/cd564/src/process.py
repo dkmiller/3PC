c227e429037156e1d859c8d33d4d074e0791401f
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2016-10-02 18:43:37 -0400

    socket mesh created

--- sample_submission/cd564/src/process.py
+++ sample_submission/cd564/src/process.py
@@ -6,60 +6,108 @@ from threading import Thread, Lock
 root_port = 20000
 address = 'localhost'
 processes = dict()
+threads = dict()
+conns = dict()
+
+class ListenThread(Thread):
+  def __init__(self):
+    Thread.__init__(self, conn, addr)
+    self.conn = conn
+    self.addr = addr
+
+  # From Daniel
+  def run(self):
+    while True:
+      data = self.conn.recv(1024):
+      do_on_receive(data)
+
+class WorkerThread(Thread):
+  def __init__(self, address, internal_port, pid):
+    Thread.__init__(self)
+    self.sock = socket(AF_INET, SOCK_STREAM)
+    self.sock.bind((address, internal_port))
+    self.sock.listen(1)
+
+  def run(self):
+    global threads
+    while True:
+      conn, addr = self.sock.accept()
+      handler = ListenThread(conn, addr)
+      handler.start()
 
 class ClientHandler(Thread):
-    def __init__(self, index, address, port):
-        Thread.__init__(self)
-        self.index = index
-        self.sock = socket(AF_INET, SOCK_STREAM)
-        self.sock.connect((address, port))
-        self.valid = True
+  def __init__(self, index, address, port):
+    Thread.__init__(self)
+    self.index = index
+    self.sock = socket(AF_INET, SOCK_STREAM)
+    self.sock.connect((address, port))
+    self.valid = True
 
     def run(self):
-        global leader, threads, wait_ack
-        while self.valid:
-            try:
-                data = self.sock.recv(1024)
-                #sys.stderr.write(data)
-                line = data.split('\n')
-                for l in line:
-                    s = l.split()
-                    if len(s) < 2:
-                        continue
-                    if s[0] == 'coordinator':
-                        leader_lock.acquire()
-                        leader = int(s[1])
-                        leader_lock.release()
-                    elif s[0] == 'resp':
-                        sys.stdout.write(s[1] + '\n')
-                        sys.stdout.flush()
-                        wait_ack_lock.acquire()
-                        wait_ack = False
-                        wait_ack_lock.release()
-                    elif s[0] == 'ack':
-                        wait_ack_lock.acquire()
-                        wait_ack = False
-                        wait_ack_lock.release()
-            except:
-                print sys.exc_info()
-                self.valid = False
-                del threads[self.index]
-                self.sock.close()
-                break
+      while True:
+        a = 1 # do something
 
-    def send(self, s):
-        if self.valid:
-            self.sock.send(str(s) + '\n')
+    def send(self, msg):
+      self.sock.send(str(msg) + '\n')
 
-    def close(self):
-        try:
-            self.valid = False
-            self.sock.close()
-        except:
-            pass
+# master
+class MasterHandler(Thread):
+  def __init__(self, index, address, port):
+    Thread.__init__(self)
+    self.index = index
+    self.sock = socket(AF_INET, SOCK_STREAM)
+    self.sock.bind((address, port))
+    self.sock.listen(1)
+    self.conn, self.addr = self.sock.accept()
+    self.valid = True
+
+  def run(self):
+    global threads, conns
+    conns[-1] = self.conn
+    while self.valid:
+      try:
+        data = self.conn.recv(1024)
+        print data
+        #sys.stderr.write(data)
+#         line = data.split('\n')
+#         for l in line:
+#             s = l.split()
+#             if len(s) < 2:
+#                 continue
+#             if s[0] == 'coordinator':
+#                 leader_lock.acquire()
+#                 leader = int(s[1])
+#                 leader_lock.release()
+#             elif s[0] == 'resp':
+#                 sys.stdout.write(s[1] + '\n')
+#                 sys.stdout.flush()
+#                 wait_ack_lock.acquire()
+#                 wait_ack = False
+#                 wait_ack_lock.release()
+#             elif s[0] == 'ack':
+#                 wait_ack_lock.acquire()
+#                 wait_ack = False
+#                 wait_ack_lock.release()
+      except:
+        print sys.exc_info()
+        self.valid = False
+        del threads[self.index]
+        self.sock.close()
+        break
+
+  def send(self, s):
+    if self.valid:
+      self.sock.send(str(s) + '\n')
+
+  def close(self):
+    try:
+      self.valid = False
+      self.sock.close()
+    except:
+      pass
 
 def send(p_id, data):
-  processes[p_id].send(str(data) + '\n')
+  outgoing_conns[p_id].send(str(data) + '\n')
 
 def main():
   global address, root_port, processes
@@ -68,14 +116,25 @@ def main():
   pid = int(sys.argv[1])
   num_processes = int(sys.argv[2])
   myport = int(sys.argv[3])
+
+  # Connection with MASTER
+  handler = MasterHandler(pid, address, myport)
+  outgoing_conns[-1] = handler
+  handler.start()
+
+  # All incoming connections
+  handler = WorkerThread(pid, address, root_port+pid)
+  handler.start()
   
-  sock = socket(AF_INET, SOCK_STREAM)
-  sock.bind((address, myport))
-  sock.listen(1)
-  conn, addr = sock.accept()
-  while (True):
-    data = conn.recv(1024)
-    print "Ash: " + data
+  for pno in range(num_processes):
+    if pno == pid:
+      continue
+    handler = ClientHandler(pno, address, root_port+pno)
+    outgoing_conns[pno] = handler
+    handler.start()
+
+  while True:
+    a = 1
   ####processes[idval] = sock
   #f = open('backend_servers', 'r')
   # check if it is a recovered process
